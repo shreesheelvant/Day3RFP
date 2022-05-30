f89d845763276fba5f13becb789bcfa616c6e162
unknown <suntanabhagyashree@gmail.com>
2022-05-30 12:45:24 +0530

    Day3RFP

---
+++ src/Smallestelement.java
@@ -0,0 +1,13 @@
+public class Smallestelement {
+    public static void main(String[] args) {
+        int [] arr = new int [] {25, 11, 7, 75, 56};
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++)
+        {
+            if(arr[i] < min)
+                min = arr[i];
+        }
+        System.out.println("Largest element present in given array: " + min);
+    }
+}
+
